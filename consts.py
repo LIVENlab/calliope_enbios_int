8032830693ddf198c9216382ec3c315c1d36fa96
kebamba <miquel.sierra@gmail.com>
2024-10-09 10:13:59 +0200

    add consts

---
+++ consts.py
@@ -0,0 +1,11 @@
+# scenario 1. Follow today's trend
+# data from Photovoltaics Report (2024) - Fraunhofer Institute
+OPEN_TECHNOLOGY_SHARE = {'CdTe': 0.02, 'CIS': 0, 'micro-Si': 0, 'multi-Si': 0.01, 'single-Si': 0.97}
+
+ROOF_3KW_SHARE = {'single-Si': 0.97, 'CIS': 0, 'a-Si': 0, 'multi-Si': 0.01, 'CdTe': 0.02, 'ribbon-Si': 0}
+ROOF_93KW_SHARE = {'multi-Si': 0.02, 'single-Si': 0.98}
+ROOF_156KW_SHARE = {'multi-Si': 0.02, 'single-Si': 0.98}
+ROOF_280KW_SHARE = {'multi-Si': 0.02, 'single-Si': 0.98}
+# no data
+ROOF_TECHNOLOGY_SHARE = {'3kWp': 0.5, '93kWp': 0.17, '156kWp': 0.165, '280kWp': 0.165}
+
