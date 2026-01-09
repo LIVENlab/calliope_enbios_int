e80895806eb819cccff3fe85d99f4161c43f2853
kebamba <miquel.sierra@gmail.com>
2026-01-09 13:42:11 +0100

    electricity mixes substitution

--- premise_external_scenario/premise_external.py
+++ premise_external_scenario/premise_external.py
@@ -2,6 +2,7 @@ from premise import NewDatabase
 from datapackage import Package
 import bw2data as bd
 import pickle
+import wurst.searching as ws
 
 bd.projects.set_current('fossil_free_ecoinvent')
 
@@ -15,7 +16,7 @@ ndb = NewDatabase(
         {
             "model": "image",
             "pathway": "SSP2-Base",
-            "year": 2050,
+            "year": 2020,
             "external scenarios": external_scenario,
         }
     ],
@@ -26,20 +27,43 @@ ndb = NewDatabase(
 ndb.update("external")
 
 # electricity mixes substitutions
+pickle_path = ndb.scenarios[0]['database filepath']
 try:
-    with open("file.pkl", "rb") as f:
+    with open(pickle_path, "rb") as f:
         data = pickle.load(f)
 except:
     print("There is no pickled database")
 
-new_high_voltage = [a for a in data if a['name'] == 'market for electricity, high voltage (new)'
-                    and a['reference product'] == 'electricity, high voltage (new)' and a['location'] != 'World']
+# delete "World" markets
+data = [a for a in data if a['location'] != 'World']
 
-new_medium_voltage = [a for a in data if a['name'] == 'market for electricity, medium voltage (new)'
-                      and a['reference product'] == 'electricity, medium voltage (new)' and a['location'] != 'World']
+voltages = ["high", "medium", "low"]
 
-new_low_voltage = [a for a in data if a['name'] == 'market for electricity, low voltage (new)'
-                   and a['reference product'] == 'electricity, low voltage (new)' and a['location'] != 'World']
+all_voltages = [
+    a for a in data
+    if any(
+        a["name"] == f"market for electricity, {v} voltage (new)"
+        and a["reference product"] == f"electricity, {v} voltage (new)"
+        for v in voltages
+    )
+]
 
+acts_to_be_replaced = []
+for act in all_voltages:
+    act_to_be_replaced = [a for a in data if a['name'] == act['name'][:-6] and a['reference product'] == act['reference product'][:-6] and a['location'] == act['location']]
+    acts_to_be_replaced.append(act_to_be_replaced[0])
+
+counter = 0
+for replaceable_act in acts_to_be_replaced:
+    replacing_act = all_voltages[counter]
+    for act in data:
+        for exchange in ws.technosphere(act):
+            if exchange['product'] == replaceable_act['reference product'] and exchange['name'] == replaceable_act['name'] and exchange['unit'] == replaceable_act['unit'] and exchange['location'] == replaceable_act['location']:
+                exchange['product'] = replacing_act['reference product']
+                exchange['name'] = replacing_act['name']
+    counter += 1
+
+with open(pickle_path, "wb") as f:
+    pickle.dump(data, f)
 
 ndb.write_db_to_brightway('test_2')
